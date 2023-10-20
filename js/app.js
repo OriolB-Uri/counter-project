d5fefc12c3185706019acaf37504ba0b9d9dd75e
OriolB-Uri <oriolbotella@hotmail.com>
2023-10-20 12:47:04 +0200

    hecho

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
 // Tu código aquí.
+let actualValue = 0;
+let counter = document.querySelector('#counter');
+
+function suma(){
+    actualValue++;
+    counter.textContent = actualValue;
+    pintar();
+}
+
+function resta(){
+    actualValue--;
+    counter.textContent = actualValue;
+    pintar();
+}
+
+function pintar(){
+    if (actualValue < 0){
+        counter.style.color = "red";
+    } else if (actualValue > 0){
+        counter.style.color = "green";
+    } else {
+        counter.style.color = "grey";
+    }
+}
